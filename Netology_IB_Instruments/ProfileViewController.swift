985ec25a73f0d5cc458624b95c60ffb0da61e3a7
OlesyaU <1600174@mail.ru>
2022-03-11 14:44:53 +0300

    Как-то незаметно для себя и достаточно быстро сделала это задание. Все пункты выполнены.

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Олеся on 11.03.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet private  var profileView: ProfileView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
